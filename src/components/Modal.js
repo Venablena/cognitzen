a593e655a47cf06bbf99efc88706303719b38afa
Lena Venable <venablena@gmail.com>
2018-11-17 21:38:53 -0800

    refactor modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -6,24 +6,31 @@ const modalRoot = document.getElementById('modal-root');
 
 class Modal extends Component {
   render() {
-    return ReactDOM.createPortal(
+    return (
       <GameContext.Consumer>
         {({
           showModal,
           handleModalClose,
         })=> {
           if(showModal) {
-            return (
+            return ReactDOM.createPortal (
               <main className={`modal`}>
                 <button onClick={handleModalClose}>close</button>
-                <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc7rEnoej4XzwvOBS3r-bvkpWqanKFhQ5hd_lR_pxKYlkTHIw/viewform?embedded=true" width="100%" height="90%" frameBorder="0" marginHeight="0" marginWidth="0">Loading...</iframe>
-              </main>
+                <iframe
+                  src="https://docs.google.com/forms/d/e/1FAIpQLSc7rEnoej4XzwvOBS3r-bvkpWqanKFhQ5hd_lR_pxKYlkTHIw/viewform?embedded=true"
+                  width="100%"
+                  height="90%"
+                  frameBorder="0"
+                  marginHeight="0" marginWidth="0">
+                  Loading...
+                </iframe>
+              </main> ,
+              modalRoot,
             )
           }
           return null;
         }}
-      </GameContext.Consumer>,
-      document.body,
+      </GameContext.Consumer>
     );
   }
 }
