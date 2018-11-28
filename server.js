c0c51a39d139a89bdcdedc662eab849a30ff59cc
Lena Venable <venablena@gmail.com>
2018-11-28 12:20:39 -0800

    fetch user data from wix in localhost server

--- server.js
+++ server.js
@@ -1,41 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const server = express();
+const app = express();
 const axios = require('axios');
 const port = process.env.PORT || 5000;
 
-server.use(bodyParser.json());
-server.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-    // POST TO WIX DATABASE //
-    // UrlFetchApp.fetch('https://www.cognitzen.com/_functions/user', {	"headers": {
-    //   "Access-Control-Allow-Origin": "*",
-		// 	"Content-Type": "application/json"
-		// }});
-
-server.get('/api/hello', (req, res) => {
-  // axios.get('https://www.cognitzen.com/_functions/user')
-  //   .then((result) => {
-  //     console.log(result.data);
-  //     //res.send(result)
-  //   })
-  //   .catch(err => {
-  //     console.log(err);
-  //   })
-  res.send({ express: 'Hello From Express' });
+app.get('/', (req, res) => {
+  axios.get('https://www.cognitzen.com/_functions/user')
+    .then((result) => {
+      res.send(result.data.items)
+    })
+    .catch((err) => {
+      res.send({err: "Couldn't fetch users from WIX API"});
+    })
 });
 
-server.post('/api/world', (req, res) => {
-  console.log(req.body);
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-
-server.listen(port, () => console.log(`Listening on port ${port}`));
-
-
-// , {	"headers": {
-//   "Access-Control-Allow-Origin": "*",
-//   "Content-Type": "application/json"
-// }}
+app.listen(port, () => console.log(`Listening on port ${port}`));
