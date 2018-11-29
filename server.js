ac92e3e13eaacc5e1dc33f6214ad30e17743aed6
Lena Venable <venablena@gmail.com>
2018-11-28 18:36:44 -0800

    more fetching

--- server.js
+++ server.js
@@ -8,15 +8,27 @@ const wixURL = 'https://www.cognitzen.com/_functions/user';
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/users/:userEmail', (req, res) => {
+  const userEmail = req.params.userEmail;
+
+  axios.get(wixURL)
+    .then((result) => {
+      const users = result.data.items;
+      const existingUser = users.find(el => el.email === userEmail);
+      res.send(existingUser);
+    })
+    .catch((err) => {
+      res.send({ err: "Couldn't fetch users from Wix API" });
+    })
+});
+
 app.get('/users', (req, res) => {
   axios.get(wixURL)
     .then((result) => {
-      // const users = result.data.items;
-      // return users.find(el => el.email === userEmail);
       res.send(result.data.items)
     })
     .catch((err) => {
-      res.send({ err: "Couldn't fetch users from WIX API" });
+      res.send({ err: "Couldn't fetch users from Wix API" });
     })
 });
 
